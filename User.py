f74587982ad4673e2df1a16db23d9a47d4c5fe25
golfdcd <68695900+golfdcd@users.noreply.github.com>
2021-07-21 09:42:16 +0700

    Create User.py

---
+++ User.py
@@ -0,0 +1,10 @@
+#เรียงลำดับชื่อ
+student = ("สมพร","แก้ว","จอมขวัญ","อัมพร","ก้อง","กล้า")
+student = input("Enter student:")
+
+print ("student is: " + student)
+student.sort()
+print("student is: " + student)
+student.reverse()
+print("student is: " + student)
+
